7d099a5b70a8e02bd7affafe550286fef3ed5fbf
Nils Hollmer <n.hollmer@gmail.com>
2020-11-12 14:55:17 +0100

    Move IPValidator functionality to model class and add it to di

--- src/IPValidator/IPValidatorController.php
+++ src/IPValidator/IPValidatorController.php
@@ -14,51 +14,6 @@ class IPValidatorController implements ContainerInjectableInterface
     use ContainerInjectableTrait;
 
     /**
-     * Use regular expression to check if input matches ip4 or ip6 syntax
-     *
-     * @param string
-     *
-     * @return string
-     */
-    public static function pregMatchIP($ip)
-    {
-        $ip4regex = "((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$))";
-        $ip6regex = "((^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))";
-
-        // Check if $ip matches ip4 syntax
-        if (preg_match($ip4regex, $ip)) {
-            return "ip4";
-        }
-
-        // Check if $ip matches ip6 syntax
-        if (preg_match($ip6regex, $ip)) {
-            return "ip6";
-        }
-
-        return "";
-    }
-
-
-    /**
-     * Check if incomping IP-address is valid
-     *
-     * @param string
-     *
-     * @return array
-     */
-    public static function validateIP($ip)
-    {
-        $match = self::pregMatchIP($ip);
-        $domain = $match ? gethostbyaddr($ip) : null;
-        return $body = [
-            "ip" => $ip,
-            "match" => $match ? "Adressen är en giltig ${match}-adress!" : "Adressen är ogiltig!",
-            "domain" => $domain
-        ];
-    }
-
-
-    /**
      * Display the stylechooser with details on current selected style.
      *
      * @return object
@@ -69,7 +24,9 @@ class IPValidatorController implements ContainerInjectableInterface
 
         $page = $this->di->get("page");
         $ip = $this->di->get("request")->getGet("ip", null);
-        $data = self::validateIP($ip);
+        $ipvalidator = $this->di->get("ipvalidator");
+        
+        $data = $ipvalidator->validateIP($ip);
 
         $page->add("nihl/ip-validator/index", $data);
 
