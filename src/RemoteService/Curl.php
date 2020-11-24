9f959e060574c01cbbdf4bb98510bb40222da429
Nils Hollmer <n.hollmer@gmail.com>
2020-11-24 22:20:09 +0100

    Quote multicurl-function until further development and remove json_decode

--- src/RemoteService/Curl.php
+++ src/RemoteService/Curl.php
@@ -2,10 +2,14 @@
 
 namespace Nihl\RemoteService;
 
-
+/**
+ * A model class for handling curl requests
+ *
+ * @SuppressWarnings(PHPMD.ShortVariable)
+ */
 class Curl
 {
-    public function request(string $url)
+    public function doRequest(string $url)
     {
         //  Initiate curl handler
         $ch = curl_init();
@@ -22,47 +26,47 @@ class Curl
         // Closing
         curl_close($ch);
 
-        // return $data;
-        return json_decode($data, true);
+        return $data;
+        // return json_decode($data, true);
     }
 
-    public function multiRequest(string $url)
-    {
-        $options = [
-            CURLOPT_RETURNTRANSFER => true,
-        ];
-
-        // Add all curl handlers and remember them
-        // Initiate the multi curl handler
-        $mh = curl_multi_init();
-        $chAll = [];
-        foreach ($userIds as $id) {
-            $ch = curl_init("$url/$id");
-            curl_setopt_array($ch, $options);
-            curl_multi_add_handle($mh, $ch);
-            $chAll[] = $ch;
-        }
-
-        // Execute all queries simultaneously,
-        // and continue when all are complete
-        $running = null;
-        do {
-            curl_multi_exec($mh, $running);
-        } while ($running);
-
-        // Close the handles
-        foreach ($chAll as $ch) {
-            curl_multi_remove_handle($mh, $ch);
-        }
-        curl_multi_close($mh);
-
-        // All of our requests are done, we can now access the results
-        $response = [];
-        foreach ($chAll as $ch) {
-            $data = curl_multi_getcontent($ch);
-            $response[] = json_decode($data, true);
-        }
-
-        return $response;
-    }
+    // public function doMultiRequest(string $url)
+    // {
+    //     $options = [
+    //         CURLOPT_RETURNTRANSFER => true,
+    //     ];
+    //
+    //     // Add all curl handlers and remember them
+    //     // Initiate the multi curl handler
+    //     $mh = curl_multi_init();
+    //     $chAll = [];
+    //     foreach ($userIds as $id) {
+    //         $ch = curl_init("$url/$id");
+    //         curl_setopt_array($ch, $options);
+    //         curl_multi_add_handle($mh, $ch);
+    //         $chAll[] = $ch;
+    //     }
+    //
+    //     // Execute all queries simultaneously,
+    //     // and continue when all are complete
+    //     $running = null;
+    //     do {
+    //         curl_multi_exec($mh, $running);
+    //     } while ($running);
+    //
+    //     // Close the handles
+    //     foreach ($chAll as $ch) {
+    //         curl_multi_remove_handle($mh, $ch);
+    //     }
+    //     curl_multi_close($mh);
+    //
+    //     // All of our requests are done, we can now access the results
+    //     $response = [];
+    //     foreach ($chAll as $ch) {
+    //         $data = curl_multi_getcontent($ch);
+    //         $response[] = json_decode($data, true);
+    //     }
+    //
+    //     return $response;
+    // }
 }
